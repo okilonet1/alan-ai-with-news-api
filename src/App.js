421bd59de3f9d92d8839f7689ba1e7e3bcfca60d
James Onyekachi <okilonet1@gmail.com>
2022-07-07 15:24:22 +0100

    fix error

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
 
   useEffect(() => {
     alanBtn({
-      key: "c4aaf4683952ff7f3d7b63ae702e0e962e956eca572e1d8b807a3e2338fdd0dc/stage",
+      key: "64370f4c903e66c5b517887fefa45c1b2e956eca572e1d8b807a3e2338fdd0dc/stage",
       onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
