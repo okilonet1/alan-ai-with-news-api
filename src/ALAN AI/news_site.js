822dba029c86834aa5b0159955a5e8f3a90fcf5f
James Onyekachi <29162889+okilonet1@users.noreply.github.com>
2022-07-07 14:27:14 +0100

    JSON parsing

--- src/ALAN AI/news_site.js
+++ src/ALAN AI/news_site.js
@@ -11,4 +11,14 @@ intent(
 const API_KEY = "16a8e5fc6f0b4d69bb82357f1ff92546";
 
 // News by Source
-intent("Give me the news from $(source* (.*))");
+intent("Give me the news from $(source* (.*))", (p) =>{
+    let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?/apiKey=${API_KEY}`;
+    
+    if(p.source.value){
+        NEWS_API_URL = `${NEWS_API_URL}&sources=${p.source.value.toLowerCase().split(" ").join('-')}`
+    }
+    
+    api.request(NEWS_API_URL, (error, response, body) => {
+        const {articles} = JSON.parse(body);
+    })
+});
