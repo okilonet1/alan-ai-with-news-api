3d6317710beadc85df6fa7c06f9df92e6155c0fe
James Onyekachi <okilonet1@gmail.com>
2022-07-07 14:13:29 +0100

    intent news from source

--- src/ALAN AI/news_site.js
+++ src/ALAN AI/news_site.js
@@ -1,8 +1,14 @@
-intent('what does this app do?', 'what can i do here?',
-      reply('This is a news app.'));
+intent(
+  "what does this app do?",
+  "what can i do here?",
+  reply("This is a news app.")
+);
 
-intent('Start a command', (p) => {
-    p.play({ command: 'testCommand'})
-})
+// intent('Start a command', (p) => {
+//     p.play({ command: 'testCommand'})
+// })
 
-const API_KEY = '16a8e5fc6f0b4d69bb82357f1ff92546';
+const API_KEY = "16a8e5fc6f0b4d69bb82357f1ff92546";
+
+// News by Source
+intent("Give me the news from $(source* (.*))");
